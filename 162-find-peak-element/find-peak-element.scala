fb6344c4d28f3b195735a7a00613ec1aba10d59c
Hriday Patgiri <51878066+hriday-the-13th@users.noreply.github.com>
2026-07-14 13:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 59.2 MB (50.00%) - LeetSync

--- 162-find-peak-element/find-peak-element.scala
+++ 162-find-peak-element/find-peak-element.scala
@@ -1,24 +1,56 @@
 object Solution {
+
     def findPeakElement(nums: Array[Int]): Int = {
-        var peak = Int.MinValue
         if(nums.length == 1){
             return 0
         }
 
-        for(i <- 1 until nums.length-1){
-            if(nums(i-1) < nums(i) && nums(i) > nums(i+1)){
-                peak = i
+        if(nums(0) > nums(1)){
+            return 0
+        }
+
+        if(nums(nums.length-1) > nums(nums.length-2)){
+            return nums.length-1
+        }
+
+        def binary(start: Int, end: Int): Int = {
+            if(start>end) return -1
+            val mid = (start + end) / 2 
+
+            if(nums(mid) > nums(mid-1) && nums(mid) > nums(mid + 1)){
+                return mid
+            }
+
+            if(nums(mid)>nums(mid-1)){
+                binary(mid+1, end)
+            } else {
+                binary(start, mid-1)
             }
         }
 
-        if(peak == Int.MinValue && nums(0) > nums(1)){
-            peak = 0
-        }
-
-        if(peak == Int.MinValue && nums(nums.length-1) > nums(nums.length-2)){
-            peak = nums.length-1
-        }
-
-        peak
+        binary(1, nums.length-2)
     }
+    //brute force
+    // def findPeakElement(nums: Array[Int]): Int = {
+    //     var peak = -1
+    //     if(nums.length == 1){
+    //         return 0
+    //     }
+
+    //     for(i <- 1 until nums.length-1){
+    //         if(nums(i-1) < nums(i) && nums(i) > nums(i+1)){
+    //             peak = i
+    //         }
+    //     }
+
+    //     if(peak == -1 && nums(0) > nums(1)){
+    //         peak = 0
+    //     }
+
+    //     if(peak == -1 && nums(nums.length-1) > nums(nums.length-2)){
+    //         peak = nums.length-1
+    //     }
+
+    //     peak
+    // }
 }
