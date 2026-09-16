20d2da4dd67f6d110a312b8511682a62ab06bec7
Hriday Patgiri <51878066+hriday-the-13th@users.noreply.github.com>
2026-09-16 13:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (97.85%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums.length == 1){
+            if(target <= nums[0]){
+                return 0;
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            if(target <= nums[i]){
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+}
