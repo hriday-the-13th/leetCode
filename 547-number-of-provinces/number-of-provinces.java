fa04883c23135b0fe10f5ed84b7e40897c68bcde
Hriday Patgiri <51878066+hriday-the-13th@users.noreply.github.com>
2026-09-17 16:20:31 +0530

    Time: 1 ms (85.59%) | Memory: 47 MB (87.66%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,41 +1,26 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> adjList = new ArrayList<>();
-
-        for(int i=0; i<isConnected.length; i++){
-            adjList.add(new ArrayList<>());
-        }
-
-        for(int i=0; i<isConnected.length; i++){
-            for(int j=0; j<isConnected[i].length; j++){
-                if(isConnected[i][j] == 1){
-                    adjList.get(i).add(j);
-                    adjList.get(j).add(i);
-                }
-            }
-        }
-
         boolean[] isVisited = new boolean[isConnected.length];
         int count = 0;
         for(int i=0; i<isConnected.length; i++){
             if(!isVisited[i]){
                 count++;
-                bfs(adjList, i, isVisited);
+                bfs(isConnected, i, isVisited);
             }
         }
         return count;
     }
-    public void bfs(List<List<Integer>> adjList, int i, boolean[] isVisited){
+    public void bfs(int[][] isConnected, int i, boolean[] isVisited){
         Queue<Integer> queue = new LinkedList<>();
         queue.add(i);
         isVisited[i] = true;
 
         while(!queue.isEmpty()){
             int node = queue.poll();
-            for(int next: adjList.get(node)){
-                if(!isVisited[next]){
-                    queue.add(next);
-                    isVisited[next] = true;
+            for(int j=0; j<isConnected[node].length; j++){
+                if(isConnected[node][j] == 1 && !isVisited[j]){
+                    queue.add(j);
+                    isVisited[j] = true;
                 }
             }
         }
