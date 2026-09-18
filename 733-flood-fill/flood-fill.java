fad453c9ea2fe03318abb619f972436db899a81c
Hriday Patgiri <51878066+hriday-the-13th@users.noreply.github.com>
2026-09-18 11:03:30 +0530

    Time: 1 ms (39.36%) | Memory: 46.8 MB (61.57%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,44 +1,43 @@
 class Solution {
-    public record Triplet(int row, int col, int color){}
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n = image.length;
         int m = image[0].length;
-        int[][] visited = new int[n][m];
 
-        for(int i=0; i<n; i++){
-            Arrays.fill(visited[i], 0);
+        int originalColor = image[sr][sc];
+
+        // Prevent revisiting forever when color doesn't change
+        if (originalColor == color) {
+            return image;
         }
 
-        Triplet src = new Triplet(sr, sc, image[sr][sc]);
-
-        bfs(image, src, color, visited);
-
-        return image;
-    }
-
-    public void bfs(int[][] image, Triplet triplet, int color, int[][] visited){
-        int n = image.length;
-        int m = image[0].length;
-        Queue<Triplet> queue = new LinkedList<>();
-        queue.add(triplet);
-        visited[triplet.row][triplet.col] = 1;
-        image[triplet.row][triplet.col] = color;
         int[] drow = {1, 0, -1, 0};
         int[] dcol = {0, 1, 0, -1};
-        while(!queue.isEmpty()){
-            Triplet curr = queue.poll();
-            int row = curr.row;
-            int col = curr.col;
-            int prevColor = curr.color;
-            for(int i=0; i<4; i++){
-                int nrow = row + drow[i];
-                int ncol = col + dcol[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == prevColor && visited[nrow][ncol] != 1){
-                    image[nrow][ncol] = color;
-                    queue.add(new Triplet(nrow, ncol, prevColor));
-                    visited[nrow][ncol] = 1;
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{sr, sc});
+
+        image[sr][sc] = color;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int row = curr[0];
+            int col = curr[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nr = row + drow[i];
+                int nc = col + dcol[i];
+
+                if (nr >= 0 && nr < n &&
+                    nc >= 0 && nc < m &&
+                    image[nr][nc] == originalColor) {
+
+                    // Mark visited immediately
+                    image[nr][nc] = color;
+                    queue.offer(new int[]{nr, nc});
                 }
             }
         }
+
+        return image;
     }
 }
