fa80d3573bab431039454927d3f4f3743e4a5173
Hriday Patgiri <51878066+hriday-the-13th@users.noreply.github.com>
2026-09-24 13:59:09 +0530

    Time: 1 ms (99.77%) | Memory: 42.6 MB (42.82%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0 || x == 1){
+            return x;
+        }
+
+        int start = 1;
+        int end = x;
+
+
+        while(start<=end){
+            int mid = start + (end - start) / 2;
+
+            if((long) mid * mid < (long) x){
+                start = mid + 1;
+            } else if(mid * mid == x){
+                return mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return end;
+
+    }
+}
